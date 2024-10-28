87a799fb38a0624f9b38d7790a542a4ab50d213f
ErikaFuerte <erika@bpoc.co.jp>
2024-10-28 10:37:03 +0800

    render

--- todo_core/settings.py
+++ todo_core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#k+s#e^pe@=*^h2r5#5znqlxq#z647!!tlr+c0q9pjzpasrfm^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
