bfc79b31ef0095570bfcdfebc5abb2ad67fd3cf6
ErikaFuerte <erika@bpoc.co.jp>
2024-10-28 09:58:38 +0800

    requirement.txt

--- build.sh
+++ build.sh
@@ -2,22 +2,13 @@
 # exit on error
 set -o errexit
 
-# Update and install necessary system packages
-apt-get update && apt-get install -y gcc libc-dev tzdata
-
-# Upgrade pip and setuptools
-pip install --upgrade pip setuptools
-
-# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
+python manage.py 
 
-# Run migrations
-python manage.py migrate
-
-# Create superuser if the CREATE_SUPERUSER environment variable is set
-if [[ $CREATE_SUPERUSER ]]; then
-  python manage.py createsuperuser --no-input
+if [[ $CREATE_SUPERUSER ]];
+then
+  python ./manage.py createsuperuser --no-input
 fi
+
